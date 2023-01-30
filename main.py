13ca1ebd6a1b6a3bbbd71a9219e6b275ff49864b
ThoradTM <pheonixgamming@gmail.com>
2023-01-30 13:41:51 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+
+chrome_options = Options()
+#point this towards your chrome browser executable
+#chrome_options.binary_location = "C:\seleniumbin\winbin\chrome-win\chrome.exe"
+#point this to the web driver that matches the version of your chrome executable
+#driver = webdriver.Chrome("C:\seleniumbin\winbin\chromedriver_win32\chromedriver.exe", options=chrome_options)
+
+#SOME VERSIONS OF CHROME WILL NOT WORK
+
+driver = webdriver.Chrome()
+
+username = input("What is your username?")
+password = input("What is your password?")
+
+login = "https://mymav.utshare.utsystem.edu/psp/ARCSPRD/EMPLOYEE/SA/c/NUI_FRAMEWORK.PT_LANDINGPAGE.GBL"
+
+calendar = "https://mymav.utshare.utsystem.edu/psc/ARCSPRD_8/EMPLOYEE/PSFT_ACS/c/UTA_FLUID_TILES.UTA_CLASS_SCHED_FL.GBL?Page=UTA_CLASS_SCHED_FL&Action=U"
+
+driver.get(login)
+time.sleep(1)
+driver.find_element(By.NAME, "loginfmt").send_keys(username)
+time.sleep(1)
+driver.find_element(By.ID, "idSIButton9").click()
+time.sleep(1)
+driver.find_element(By.NAME, "passwd").send_keys(password)
+time.sleep(1)
+driver.find_element(By.ID, "idSIButton9").click()
+time.sleep(1)
+driver.get(calendar)
+time.sleep(1)
+calendarText = driver.find_element(By.ID, "PT_MAIN")
+time.sleep(1)
+print(calendarText.text)
+
+driver.close()
