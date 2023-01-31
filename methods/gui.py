92c9cc5ef3f1dc53302b631fd3af0ef413d86f96
ThoradTM <pheonixgamming@gmail.com>
2023-01-30 18:11:11 -0600

    Added base gui code, soon to add text boxes.

--- methods/gui.py
+++ methods/gui.py
@@ -1,13 +1,15 @@
-from PyQt6.QtWidgets import QApplication, QWidget
 import sys
 
-# Only one QApplication instance is needed for this program
-# sys.argv passes commandline arguments to it
-app = QApplication(sys.argv)
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
-window = QWidget()
-window.show()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setMinimumSize(QSize(960, 540))
+        self.setWindowTitle("CSPlanner")
+        button = QPushButton("Hello World!")
 
-app.exec()
+        self.setCentralWidget(button)
